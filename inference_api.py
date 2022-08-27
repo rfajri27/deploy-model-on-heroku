0227faa43661984b673cab1c137bc07c9f86951f
rfajri27 <rfajri255@gmail.com>
2022-08-28 00:57:03 +0700

    update

--- inference_api.py
+++ inference_api.py
@@ -39,12 +39,9 @@ class RowData(BaseModel):
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    # os.system('rm -rf .dvc/cache')
-    # os.system('rm -rf .dvc/tmp/lock')
-    # os.system('dvc config core.hardlink_lock true')
-    if os.system("dvc pull -q") != 0:
+    if os.system("dvc pull") != 0:
         exit("dvc pull failed")
-    os.system("rm -rf .dvc .apt/usr/lib/dvc")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 @app.get("/")
